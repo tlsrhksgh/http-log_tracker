1dbccc90098b458a3ead7133db89d2fa45f4c5a0
tlsrhksgh <sin99145@naver.com>
2023-07-03 23:57:15 +0900

    import 정리

--- src/main/java/hello/advanced/app/v4/OrderControllerV4.java
+++ src/main/java/hello/advanced/app/v4/OrderControllerV4.java
@@ -1,10 +1,8 @@
 package hello.advanced.app.v4;
 
-import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.template.AbstractTemplate;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
